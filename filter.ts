5177c1c8a37b9da1db24f9bc65d07f77d248fa30
Taehyeon Kim <1234tgk@gmail.com>
2020-05-02 13:17:16 -0400

    filter draft

--- filter.ts
+++ filter.ts
@@ -1,21 +1,20 @@
 import { Course } from "./data";
 
 export function filter(nameArray: Array<string>, courseArray: Array<Course>): Array<Course> {
+    let ret: Array<Course> = [];
     let courseArraySize: number = courseArray.length;
-    let i: number = 0;
-    while (i < courseArraySize) {
+    for (let i: number = 0; i < courseArraySize ; i++) {
         let flag: boolean = false;
         let course: Course = courseArray[i];
-        let courseName: string = course[name];
+        let courseName: string = course.name;
 
         let nameArraySize = nameArray.length;
         for (let j: number = 0 ; j < nameArraySize && !flag ; j++) {
             if (nameArray[j] == courseName) flag = true;
         }
 
-        if (flag == true) i++;
-        else courseArray.splice(i, 1);
+        if (flag == true) ret.push(course);
     }
 
-    return courseArray;
+    return ret;
 }
