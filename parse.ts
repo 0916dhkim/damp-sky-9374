d91608ea32f3f1dc12c4bb5930b4fd5b4a10905f
Taehyeon Kim <1234tgk@gmail.com>
2020-05-02 14:03:18 -0400

    parse to hashmap draft

--- parse.ts
+++ parse.ts
@@ -1,4 +1,9 @@
 import { Course } from "./data";
-export function parseCourses(jsonString: string): Array<Course> {
-    return JSON.parse(jsonString);
+export function parseCourses(jsonString: string): Map<string, Course> {
+    let obj: Array<Course> = JSON.parse(jsonString);
+    let ret: Map<string, Course> = new Map();
+    for (let i: number = 0 ; i < obj.length ; i++) {
+        ret.set(obj[i].name, obj[i]);
+    }
+    return ret;
 }
