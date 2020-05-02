e4a395ab72772c831abe3c5b4538bdd7cdc0c141
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-02 14:41:57 -0400

    Add Schedule Permutation Structure

---
+++ permute.ts
@@ -0,0 +1,11 @@
+import { Course, Schedule } from "./data";
+
+/**
+ * Calculate schedule permutation.
+ * @param courses Courses to be permuted.
+ * @returns Array of all possible schedules that can be built from courses.
+ */
+export function permuteSchedule(courses: Map<string, Course>): Array<Schedule> {
+    // TODO: Implement.
+    return [];
+}
---
+++ schedule.ts
@@ -0,0 +1,22 @@
+import { Schedule, ScheduleElement } from "./data";
+
+/**
+ * Add an element to a schedule.
+ * Do not modify the original.
+ * @param schedule Original schedule.
+ * @param element Element to be added.
+ */
+export function addScheduleElement(schedule: Schedule, element: ScheduleElement): Schedule {
+    // TODO: Implement.
+    return [];
+}
+
+/**
+ * Check if the given schedule has collisions.
+ * @param schedule 
+ * @returns True if collides.
+ */
+export function checkScheduleCollision(schedule: Schedule): boolean {
+    // TODO: Implement.
+    return false;
+}
