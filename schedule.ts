ede69068416a33a00a004cf0aedc92f908aef6e8
Taehyeon Kim <1234tgk@gmail.com>
2020-05-02 15:23:47 -0400

    schedule draft

--- schedule.ts
+++ schedule.ts
@@ -3,12 +3,22 @@ import { Schedule, ScheduleElement } from "./data";
 /**
  * Add an element to a schedule.
  * Do not modify the original.
+ * Assume the given schedule is sorted.
  * @param schedule Original schedule.
  * @param element Element to be added.
  */
 export function addScheduleElement(schedule: Schedule, element: ScheduleElement): Schedule {
-    // TODO: Implement.
-    return [];
+    let ret: Schedule = [];
+    let scheduleSize: number = schedule.length;
+    let flag: boolean = false;
+    for (let i: number = 0 ; i < scheduleSize ; i++) {
+        if (schedule[i].startTime >= element.startTime && !flag) {
+            ret.push(schedule[i]);
+            flag = true;
+        } // includes when there's a duplicate start time
+        ret.push(schedule[i]);
+    }
+    return ret;
 }
 
 /**
@@ -17,6 +27,9 @@ export function addScheduleElement(schedule: Schedule, element: ScheduleElement)
  * @returns True if collides.
  */
 export function checkScheduleCollision(schedule: Schedule): boolean {
-    // TODO: Implement.
+    let scheduleSize: number = schedule.length;
+    for (let i: number = 1; i < scheduleSize ; i++) {
+        if (schedule[i].startTime < schedule[i - 1].endTime) return true;
+    }
     return false;
 }
