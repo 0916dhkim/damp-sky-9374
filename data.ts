8826e1c11ad52443e5f7df6e175e80b138ba2c36
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-02 10:48:54 -0400

    Define Data

--- data.ts
+++ data.ts
@@ -1,3 +1,15 @@
+export interface Time {
+    hour: number;
+    minute: number;
+}
+
+export interface Slot {
+    day: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun";
+    startTime: Time;
+    endTime: Time;
+}
+
 export interface Course {
-    // TODO: Define course interface.
+    name: string;
+    options: Array<Array<Slot>>;
 }
