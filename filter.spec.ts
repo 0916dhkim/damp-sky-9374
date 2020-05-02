cb0fe62d6023be27803ba48762b663377f3fcd6f
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-02 13:45:07 -0400

    Use Map For Course Collection

--- filter.spec.ts
+++ filter.spec.ts
@@ -2,30 +2,35 @@ import { filter } from "./filter";
 import { expect } from "chai";
 import { Course } from "./data";
 
-describe("filter", function() {
-    it("Zero Courses", function() {
-        expect(filter(["donotmatter"], [])).to.deep.equals([]);
+describe("filter", function () {
+    it("Zero Courses", function () {
+        expect(filter(["donotmatter"], new Map())).to.deep.equals(new Map());
     });
-    it("One Out Of Three", function() {
+    it("One Out Of Three", function () {
         const names = ["MA101"];
-        const courses: Array<Course> = [
-            {
+        const courses: Map<string, Course> = new Map([
+            ["EECS403", {
                 name: "EECS403",
                 options: []
-            },
-            {
+            }],
+            ["MA101", {
                 name: "MA101",
                 options: []
-            },
-            {
+            }],
+            ["ENG120", {
                 name: "ENG120",
                 options: []
-            }
-        ];
+            }]
+        ]);
 
-        expect(filter(names, courses)).deep.equals([{
-            name: "MA101",
-            options: []
-        }]);
+        expect(filter(names, courses)).deep.equals(new Map(
+            [[
+                "MA101",
+                {
+                    name: "MA101",
+                    options: []
+                }
+            ]]
+        ));
     });
 });
--- parse.spec.ts
+++ parse.spec.ts
@@ -5,7 +5,7 @@ import { Course } from "./data";
 describe("parseCourses", function() {
     it("Empty Array", function() {
         const actual = parseCourses("[]");
-        expect(actual).to.deep.equals([]);
+        expect(actual).to.deep.equals(new Map());
     })
 
     it("Basic Courses", function() {
@@ -72,68 +72,74 @@ describe("parseCourses", function() {
             }
         ]`;
         const actual = parseCourses(input);
-        const expected: Array<Course> = [
-            {
-                "name": "MA201",
-                "options": [
-                    [
-                        {
-                            "day": "Mon",
-                            "startTime": {
-                                "hour": 11,
-                                "minute": 30
-                            },
-                            "endTime": {
-                                "hour": 12,
-                                "minute": 30
+        const expected: Map<string, Course> = new Map([
+            [
+                "MA201",
+                {
+                    "name": "MA201",
+                    "options": [
+                        [
+                            {
+                                "day": "Mon",
+                                "startTime": {
+                                    "hour": 11,
+                                    "minute": 30
+                                },
+                                "endTime": {
+                                    "hour": 12,
+                                    "minute": 30
+                                }
                             }
-                        }
+                        ]
                     ]
-                ]
-            },
-            {
-                "name": "MA202",
-                "options": [
-                    [
-                        {
-                            "day": "Tue",
-                            "startTime": {
-                                "hour": 17,
-                                "minute": 45
-                            },
-                            "endTime": {
-                                "hour": 20,
-                                "minute": 45
+                }
+            ],
+            [
+                "MA202",
+                {
+                    "name": "MA202",
+                    "options": [
+                        [
+                            {
+                                "day": "Tue",
+                                "startTime": {
+                                    "hour": 17,
+                                    "minute": 45
+                                },
+                                "endTime": {
+                                    "hour": 20,
+                                    "minute": 45
+                                }
                             }
-                        }
-                    ],
-                    [
-                        {
-                            "day": "Wed",
-                            "startTime": {
-                                "hour": 12,
-                                "minute": 30
+                        ],
+                        [
+                            {
+                                "day": "Wed",
+                                "startTime": {
+                                    "hour": 12,
+                                    "minute": 30
+                                },
+                                "endTime": {
+                                    "hour": 13,
+                                    "minute": 30
+                                }
                             },
-                            "endTime": {
-                                "hour": 13,
-                                "minute": 30
+                            {
+                                "day": "Fri",
+                                "startTime": {
+                                    "hour": 9,
+                                    "minute": 0
+                                },
+                                "endTime": {
+                                    "hour": 11,
+                                    "minute": 0
+                                }
                             }
-                        },
-                        {
-                            "day": "Fri",
-                            "startTime": {
-                                "hour": 9,
-                                "minute": 0
-                            },
-                            "endTime": {
-                                "hour": 11,
-                                "minute": 0
-                            }
-                        }
+                        ]
                     ]
-                ]
-            }
-        ];
+                }
+            ]
+        ]);
         expect(actual).to.deep.equals(expected);
     })
 })
