3958b1d1a07329a67d3d4e5e5a895e0d70bc99fa
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-03 01:42:00 -0400

    Generate Random Courses

---
+++ data.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * Readonly array containing all days starting from Monday.
+ */
+exports.ALL_DAYS = [
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun"
+];
+;
